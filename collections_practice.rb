d810c15b9960876edce80ab7b8583da6e53643cb
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-25 05:58:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,3 +36,22 @@ def add_a(array)
     word.chr =="a"
   end
 end
+
+
+def sum_array (array)
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+
+
+
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
