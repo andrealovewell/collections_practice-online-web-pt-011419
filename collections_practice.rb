ed9341963045e9b96b965fcd1f12fe84d0b6fe90
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-25 05:52:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,24 @@ def sort_array_desc(array)
     y <=> x
   end
 end
+
+def sort_array_char_count (array)
+  array.sort |x,y| do
+    x <=>y
+  end
+end
+
+def swap_elements(array)
+  array[1], array [2] = array[2], array[1]
+end
+
+def reverse_array (array)
+  array.reverse
+end
+
+def kesha_make(array)
+  array.collect do |i|
+    i[2] = "$"
+    i
+end
+
