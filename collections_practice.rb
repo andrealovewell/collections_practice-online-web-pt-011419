3b1f9f7973e1b871b554ca91c2cb733cfeeb7526
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-25 05:58:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,9 +48,9 @@ end
 
 def add_s (array)
   array.collect.with_index do |word, index|
-    if index == 1 
-      word 
-    else 
+    if index == 1
+      word
+    else
       word + "s"
     end
   end
